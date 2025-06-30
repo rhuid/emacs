412640978c1d337e5f9d96842344fdc149d209d1
Ronald Huidrom <ronhuidrom@gmail.com>
2025-06-30 15:07:56 +0530

    Add keybindings.el

--- elisp/01-looks.el
+++ elisp/01-looks.el
@@ -1,4 +1,4 @@
-;; Looks
+;; Looks and theme
 
 ;; To set the size and positon of Emacs window at startup (may be required in Emacs client mode)
 (setq default-frame-alist
@@ -50,5 +50,47 @@
 (use-package rainbow-delimiters                              ; different color for each pair of parenthesis
   :ensure t
   :hook (prog-mode . rainbow-delimiters-mode))
+
+;;(use-package catppuccin-theme                      
+;;  :ensure t
+;;  :config
+;;  (load-theme 'catppuccin t))
+
+(use-package gotham-theme                    
+  :ensure t
+  :config
+  (load-theme 'gotham t)
+  (set-face-attribute 'font-lock-comment-face nil
+		      :slant 'oblique))                     ; make comments oblique
+;;		      :foreground "#999999"))               ; comment color (grayish)
+;;		      :weight 'light))                      ; make comments lighter
+
+;;(use-package gandalf-theme
+;;  :ensure t
+;;  :config
+;;  (load-theme 'gandalf t))
+
+;;(use-package adwaita-dark-theme
+;;  :ensure t
+;;  :config
+;;  (load-theme 'adwaita-dark))
+
+;;(use-package ample-theme
+;;  :ensure t
+;;  :config
+;;  (load-theme 'ample))
+
+;;(use-package ef-themes
+;;  :ensure t)
+;;(load-theme 'ef-spring t)
+
+;;(use-package doom-themes
+;;  :ensure t
+;;  :init                                 ; Runs before the package is loaded
+;;  (setq doom-themes-enable-bold t
+;;        doom-themes-enable-italic t)
+;;  :config                               ; Runs after the package is loaded
+;;  (load-theme 'doom-one t)
+;;  (doom-themes-org-config))             ; Improved org-mode styling
 
 (provide '01-looks)
---
+++ elisp/02-keybindings.el
@@ -0,0 +1,8 @@
+;; Keybindings
+
+(global-set-key (kbd "C-c r") #'replace-string)
+(global-set-key (kbd "C-c w") #'delete-trailing-whitespace)
+(global-set-key (kbd "C-c c") #'comment-region)
+(global-set-key (kbd "C-c u") #'uncomment-region)
+
+(provide '02-keybindings)
