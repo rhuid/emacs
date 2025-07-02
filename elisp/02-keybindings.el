a293a1f35bac0dfcb0822d4ca975303602a1aaa9
Ronald Huidrom <ronhuidrom@gmail.com>
2025-07-02 13:04:41 +0530

    upate keys

--- elisp/02-keybindings.el
+++ elisp/02-keybindings.el
@@ -2,5 +2,6 @@
 
 (global-set-key (kbd "C-c r") #'replace-string)
 (global-set-key (kbd "C-c w") #'delete-trailing-whitespace)
+(global-set-key (kbd "C-c t c") #'company-mode)
 
 (provide '02-keybindings)
