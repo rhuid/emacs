516f58d58c733aed82f2b833011d995a7e2181d1
Ronald Huidrom <ronhuidrom@gmail.com>
2025-07-16 18:36:39 +0530

    Modularized and added knot-keybindings

--- core/90-tools.el
+++ core/90-tools.el
@@ -1,7 +1,6 @@
 ;;; 90-tools.el --- just tools -*- lexical-binding: t; -*-
 
 ;; (require 'rh-capitalize)
-
 (use-package centaur-tabs :disabled t
   :demand nil
   :config
@@ -18,79 +17,6 @@
 ;; (use-package auto-complete
 ;;  :config
 ;;  (ac-config-default))
-
-;; MINIBUFFER
-
-;; UI for minibuffer candidates
-(use-package vertico :straight t :demand t :init (vertico-mode 1))
-(use-package savehist :straight nil :demand t :init (savehist-mode))
-(use-package orderless :straight t :demand t 
-  ;; Type multiple words in any order to match candidates. Fuzzy, regex, initialism, fle
-  :init
-  (setq completion-styles '(orderless)
-        completion-category-defaults nil))
-
-;; Add extra info to candidates in the minibuffer, such as docstring summaries and more
-(use-package marginalia :straight t  :demand t :init (marginalia-mode))
-(use-package consult :straight t :demand t 
-  ;; Adds modern alternatives to core Emacs commands
-  :bind
-  (("C-c f" . consult-find)
-   ("C-c l" . consult-locate)
-   ("C-c r" . consult-recent-file)
-   ("C-s" . consult-line)
-   ("C-x b" . consult-buffer)
-   ("M-y" . consult-yank-pop)
-   )
-  :config
-  ;; To always start searching from home directory
-  (advice-add 'consult-find :around
-              (lambda (orig &rest args)
-		(let ((default-directory (expand-file-name "~")))
-                  (apply orig args))))
-  )
-
-;; (setq consult-preview-key "M-.") ; preview only when you press M-
-
-;; Live popup of possible key combinations
-(use-package which-key :straight t  :demand t :config (which-key-mode))
-(use-package embark :straight t :demand t 
-  :bind
-  (("C-." . embark-act)         ;; pick some comfortable binding
-   ("C-;" . embark-dwim)        ;; good alternative: M-.
-   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
-
-  :init
-
-  ;; Optionally replace the key help with a completing-read interface
-  (setq prefix-help-command #'embark-prefix-help-command)
-
-  ;; Show the Embark target at point via Eldoc. You may adjust the
-  ;; Eldoc strategy, if you want to see the documentation from
-  ;; multiple providers. Beware that using this can be a little
-  ;; jarring since the message shown in the minibuffer can be more
-  ;; than one line, causing the modeline to move up and down:
-
-  ;; (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
-  ;; (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
-
-  ;; Add Embark to the mouse context menu. Also enable `context-menu-mode'.
-  ;; (context-menu-mode 1)
-  ;; (add-hook 'context-menu-functions #'embark-context-menu 100)
-
-  :config
-
-  ;; Hide the mode line of the Embark live/completions buffers
-  (add-to-list 'display-buffer-alist
-               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
-                 nil
-                 (window-parameters (mode-line-format . none)))))
-
-;; Consult users will also want the embark-consult package.
-(use-package embark-consult :straight t :demand t 
-  ;; only need to install it, embark loads it after consult if found
-  :hook
-  (embark-collect-mode . consult-preview-at-point-mode))
 
 ;; ESHELL
 
@@ -203,40 +129,5 @@
   (add-hook 'calc-trail-mode-hook 'evil-insert-state))
 
 (use-package sudo-edit :straight t :commands (sudo-edit))
-(use-package general :straight t :demand t :after outline
-  :config
-  (general-create-definer rh/leader-keys
-    :states '(normal visual)
-    :prefix "SPC"
-    :global-prefix "C-SPC")
-
-  (rh/leader-keys
-    ;; eshell
-    "e t"     'rh/eshell-toggle
-
-    ;; eval
-    "e e"     'eval-expression
-
-    ;; outline minor mode
-    "o t"     'rh/outline-toggle-heading
-    "o <tab>" 'rh/outline-toggle-heading
-    "o a"     'rh/outline-toggle-visibility
-
-    ;; string manipulation
-    "s r"     'replace-string
-    "s w"     'delete-trailing-whitespace
-    "s a"     'abbrev-mode
-
-    ;; vterm
-    "t t"     'rh/vterm-toggle
-    
-    ;; utilities
-    "u r"     'recentf-open-files
-    ))
-
-;; All custom keybindings
-
-(global-set-key (kbd "<f5>") #'rh/cycle-modus-themes)
-
 
 (provide '90-tools)
---
+++ core/knot-keybindings.el
@@ -0,0 +1,37 @@
+;;; knot-keybindings.el --- All keybindings with or without general.el -*- lexical-binding: t; -*-
+
+(use-package general :straight t :demand t :after outline
+  :config
+  (general-create-definer rh/leader-keys
+    :states '(normal visual)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
+
+  (rh/leader-keys
+    ;; eshell
+    "e t"     'rh/eshell-toggle
+
+    ;; eval
+    "e e"     'eval-expression
+
+    ;; outline minor mode
+    "o t"     'rh/outline-toggle-heading
+    "o <tab>" 'rh/outline-toggle-heading
+    "o a"     'rh/outline-toggle-visibility
+
+    ;; string manipulation
+    "s r"     'replace-string
+    "s w"     'delete-trailing-whitespace
+    "s a"     'abbrev-mode
+
+    ;; vterm
+    "t t"     'rh/vterm-toggle
+    
+    ;; utilities
+    "u r"     'recentf-open-files
+    ))
+
+;; All custom keybindings
+;; (global-set-key (kbd "<f5>") #'rh/cycle-modus-themes)
+
+(provide 'knot-keybindings)
