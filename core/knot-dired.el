d181ef276a059250d7f481a8963aa653d86fdc9d
Ronald Huidrom <ronhuidrom@gmail.com>
2025-07-22 23:37:19 +0530

    fix keybinding issue with dired

--- core/knot-dired.el
+++ core/knot-dired.el
@@ -49,20 +49,20 @@
 ;;       (dired-add-file full-path)
 ;;       (revert-buffer))))
 
-;; ;; Open the current directory in the GUI file manager
-;; (defun open-in-file-manager ()
-;;   "Open the current directory in the system's GUI file manager."
-;;   (interactive)
-;;   (let ((path (shell-quote-argument (expand-file-name default-directory))))
-;;     (cond ((eq system-type 'windows-nt)
-;; 	   (shell-command (concat "explorer " (replace-regexp-in-string "/" "\\" path t t))))
-;; 	  ((eq system-type 'darwin)
-;; 	   (shell-command (concat "open " path)))
-;; 	  ((eq system-type 'gnu/linux)
-;; 	   (shell-command (concat "xdg-open " path))))))
+;; Open the current directory in the GUI file manager
+(defun open-in-file-manager ()
+  "Open the current directory in the system's GUI file manager."
+  (interactive)
+  (let ((path (shell-quote-argument (expand-file-name default-directory))))
+    (cond ((eq system-type 'windows-nt)
+	   (shell-command (concat "explorer " (replace-regexp-in-string "/" "\\" path t t))))
+	  ((eq system-type 'darwin)
+	   (shell-command (concat "open " path)))
+	  ((eq system-type 'gnu/linux)
+	   (shell-command (concat "xdg-open " path))))))
 
 ;; Open files in their default application
-(defun dired-open-file ()
+(defun rh/dired-open-file ()
   "Open the file at point in its default external application, but open text files inside Emacs. If a directory is selected, enter it as usual."
   (interactive)
   (let ((file (dired-get-file-for-visit)))
@@ -85,23 +85,16 @@
 	    ((eq system-type 'gnu/linux)
 	     (shell-command (concat "xdg-open " (shell-quote-argument file)))))))))
 
-;; Key bindings
 (with-eval-after-load 'dired
-  ;; (define-key dired-mode-map (kbd "N")       ; N          create a new file
-  ;; 	      'my/dired-create-file)
-  ;; (define-key dired-mode-map (kbd "C-c o")   ; C-c o      open the current directory in GUI file manager
-  ;; 	      #'open-in-file-manager)
-  (define-key dired-mode-map (kbd "RET")     ; RET        open files in their default application
-	      #'dired-open-file))
+  (evil-define-key 'normal dired-mode-map
+    (kbd "RET")   #'rh/dired-open-file
+    ;; (kbd "N")     #'my/dired-create-file
+    (kbd "C-c o") #'open-in-file-manager))
 
 ;; Default keybindings
 ;; C-x d    dired mode
-;; ^        Move one directory up
 ;; d        Mark for delete
 ;; x        Delete the marked items
-;; D        Delete
-;; +        Create a new directory
-;; R        Rename directory/file
 ;; C        Copy
 
 (provide 'knot-dired)
