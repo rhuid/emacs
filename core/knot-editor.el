ce89534d659f872fae013d9eb9e8c6c12e360f3a
Ronald Huidrom <ronhuidrom@gmail.com>
2025-07-27 20:58:22 +0530

    syntax cleanup

--- core/knot-editor.el
+++ core/knot-editor.el
@@ -2,23 +2,23 @@
 
 (use-package evil			:straight t :demand t
   :init
-  (setq evil-want-integration t)                  ;; This is optional since it's already set to t by default
+  (setq evil-want-integration t)
   (setq evil-want-keybinding nil)
   (setq evil-visual-state-cursor 'hollow)
   (setq evil-want-Y-yank-to-eol t)
   (setq evil-disable-insert-state-bindings t)     ;; If set to t, Emacs keybindings are available in insert state
   (setq evil-undo-system 'undo-redo)              ;; Use the undo-redo system available in Emacs >= 28
+  (setq evil-mode-line-format 'before)
 
   :config
   (evil-mode 1)
-  (setq cursor-type 'box)
-  )
+  (setq cursor-type 'box))
 
-(use-package evil-collection		:straight t  :demand t :after evil
+(use-package evil-collection		:straight t :demand t :after evil
   :config
   (evil-collection-init))
 
-(use-package evil-surround		:straight t  :demand t :after evil
+(use-package evil-surround		:straight t :demand t :after evil
   :config
   (global-evil-surround-mode 1))
 
@@ -26,7 +26,7 @@
   :config
   (evil-commentary-mode))
 
-(use-package evil-nerd-commenter	:straight t  :demand t :after evil
+(use-package evil-nerd-commenter	:straight t :demand t :after evil
   :bind ("M-;" . evilnc-comment-or-uncomment-lines))
 
 (use-package evil-snipe			:straight t :demand t  :after evil
@@ -34,24 +34,13 @@
   (evil-snipe-mode +1)
   (evil-snipe-override-mode +1))
 
-(use-package evil-goggles		:straight t :disabled t
-  :config
-  (evil-goggles-mode)
-
-  ;; optionally use diff-mode's faces; as a result, deleted text
-  ;; will be highlighed with `diff-removed` face which is typically
-  ;; some red color (as defined by the color theme)
-  ;; other faces such as `diff-added` will be used for other actions
-  (evil-goggles-use-diff-faces))
-
 (use-package evil-colemak-basics	:straight t :demand t :after evil
   :init
   (setq evil-colemak-basics-layout-mod 'mod-dh)
   :config
   (global-evil-colemak-basics-mode))
 
-
-;; ;; Toggle Evil (optional: re-init evil-collection)
+;; Toggle Evil (optional: re-init evil-collection)
 ;; (defun toggle-evil ()
 ;;   "Toggle Evil mode and related extensions globally."
 ;;   (interactive)
@@ -69,32 +58,5 @@
 ;;       (evil-collection-init) ; optional: re-init collection
 ;;       (setq cursor-type 'box)
 ;;       (message "Evil mode enabled."))))
-
-;; (global-set-key (kbd "C-c e") #'toggle-evil)
-
-;; (global-set-key (kbd "C-c r") #'replace-string)
-;; (global-set-key (kbd "C-c w") #'delete-trailing-whitespace)
-;; (global-set-key (kbd "C-c t c") #'company-mode)
-;; (global-set-key (kbd "C-c t a") #'abbrev-mode)
-
-;; (defun toggle-header-line ()
-;;   "Toggle the header-line-format in the current buffer."
-;;   (interactive)
-;;   (setq header-line-format
-;;         (if header-line-format
-;;             nil
-;;           ""))
-;;   (redraw-display))  ;; optional: forces immediate refresh
-
-;; (global-set-key (kbd "C-c t h") #'toggle-header-line)
-
-;; (defun rh/clean-mode ()
-;;   "Toggle clean-mode: header line and company mode."
-;;   (interactive)
-;;   (toggle-header-line)
-;;   (company-mode 'toggle)
-;;   (flymake-mode 'toggle))
-
-;; (global-set-key (kbd "C-c c") #'rh/clean-mode)
 
 (provide 'knot-editor)
