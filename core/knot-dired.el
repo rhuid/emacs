979793027afd081a070ae5b5c0d2a77766a737c5
Ronald Huidrom <ronhuidrom@gmail.com>
2025-08-10 05:05:17 +0530

    keybindings here rather than in the knot-editor

--- core/knot-dired.el
+++ core/knot-dired.el
@@ -1,20 +1,28 @@
 ;;; knot-dired.el --- Enchancements of the already great file manager -*- lexical-binding: t; -*-
 
-(use-package dired :ensure nil
+(use-package dired
+  :ensure nil
+  :bind (:map dired-mode-map
+              ("g"    . dired-git-info-mode)
+              ("r"    . dired-up-directory)
+              ("i"    . rh/dired-open-file)
+              ("u"    . dired-unmark)
+              ("U"    . dired-unmark-all-marks)
+              ("<f5>" . revert-buffer-quick))
+
   :hook ((dired-mode . rh/dired-setup)
-	 (dired-mode . dired-hide-details-mode))
+	       (dired-mode . dired-hide-details-mode))
+
   :config
   (defun rh/dired-setup ()
     (display-line-numbers-mode -1)
     (set-window-buffer (selected-window) (current-buffer))
-    (setq-local cursor-type nil)
-
-    )
+    (setq-local cursor-type nil))
 
   (setq dired-listing-switches "-alh --group-directories-first"
-	dired-dwim-target t
-	dired-mouse-drag-files t
-	dired-recursive-copies 'always))
+	      dired-dwim-target t
+	      dired-mouse-drag-files t
+	      dired-recursive-copies 'always))
 
   (use-package all-the-icons-dired
     :hook (dired-mode . all-the-icons-dired-mode))
@@ -23,12 +31,19 @@
   :hook (dired-mode . diredfl-mode)
   :config (diredfl-global-mode 1))
 
+(use-package dired-git-info
+  :after dired
+  :bind (:map dired-mode-map
+              (")" . dired-git-info-mode))
+  :config
+  (setq dgi-auto-hide-details-p nil))
+
 (use-package dired-preview :disabled t :after dired
   :hook (dired-mode . dired-preview-mode)
   :config
   (setq dired-preview-delay 0.2
         dired-preview-max-size 10                   ;; max 10 MB
-	dired-preview-use-timer t))
+	      dired-preview-use-timer t))
 
 (use-package dired-du :disabled t
   :after dired
@@ -36,34 +51,16 @@
   :config
   (add-hook 'dired-mode-hook #'dired-du-mode))
 
-(use-package dired-git-info :after dired
-  ;; :hook (dired-after-readin . dired-git-info-auto-enable)
-  :bind (:map dired-mode-map
-              (")" . dired-git-info-mode))            ;; press `)` to toggle git info
-  :config
-  (setq dgi-auto-hide-details-p nil))
-
-;; ;; Create a new file
-;; (defun my/dired-create-file (filename)
-;;   "Create a new file named FILENAME in the current dired directory."
-;;   (interactive (list (read-file-name "New file: " (dired-current-directory))))
-;;   (let ((full-path (expand-file-name filename (dired-current-directory))))
-;;     (if (file-exists-p full-path)
-;; 	(message "File already exists")
-;;       (write-region "" nil full-path)
-;;       (dired-add-file full-path)
-;;       (revert-buffer))))
-
 (defun open-in-file-manager ()
   "Open the current directory in the system's GUI file manager."
   (interactive)
   (let ((path (shell-quote-argument (expand-file-name default-directory))))
     (cond ((eq system-type 'windows-nt)
-	   (shell-command (concat "explorer " (replace-regexp-in-string "/" "\\" path t t))))
-	  ((eq system-type 'darwin)
-	   (shell-command (concat "open " path)))
-	  ((eq system-type 'gnu/linux)
-	   (shell-command (concat "xdg-open " path))))))
+	         (shell-command (concat "explorer " (replace-regexp-in-string "/" "\\" path t t))))
+	        ((eq system-type 'darwin)
+	         (shell-command (concat "open " path)))
+	        ((eq system-type 'gnu/linux)
+	         (shell-command (concat "xdg-open " path))))))
 
 (defun rh/dired-open-file ()
   "Smart open for files in Dired:
