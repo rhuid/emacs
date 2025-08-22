1d205a3c84d13853aab9430988c4e8604e02e518
Ronald Huidrom <ronhuidrom@gmail.com>
2025-08-22 17:48:52 +0530

    minor syntax tweaks

--- core/knot-startup-page.el
+++ core/knot-startup-page.el
@@ -1,5 +1,7 @@
 ;;; knot-startup-page.el --- A minimal no-bullshit startup page -*- lexical-binding: t; -*-
 
+;;;; A random line or poem every time you start emacs!
+
 (defun rh/random-excerpt (file)
   "Return a random quote or poem from FILE, separated by lines with ---."
   (with-temp-buffer
@@ -9,26 +11,30 @@
       (when (> count 0)
         (string-trim (nth (random count) entries))))))
 
+;;;; Customize the scratch buffer
+
 (setq initial-major-mode 'fundamental-mode
       initial-scratch-message
       (with-temp-buffer
-	(insert "\nWelcome home, Ronald.\n")
-	(insert-file-contents (concat user-emacs-directory "logo/Emacs-ANSI-Shadow.txt"))
-	(goto-char (point-max))
-	(insert "\n")
-	(insert (rh/random-excerpt (concat user-emacs-directory "excerpts.txt")))
-	(insert "\n")
-	(buffer-string)))
+	      (insert "\nWelcome home, Ronald.\n")
+	      (insert-file-contents (concat user-emacs-directory "logo/Emacs-ANSI-Shadow.txt"))
+	      (goto-char (point-max))
+	      (insert "\n")
+	      (insert (rh/random-excerpt (concat user-emacs-directory "excerpts.txt")))
+	      (insert "\n")
+	      (buffer-string)))
+
+;;;; Visual customizations
 
 (add-hook 'emacs-startup-hook
-	  (lambda ()
-	    (with-current-buffer "*scratch*"
-	      (setq-local buffer-read-only   t
-			  left-margin-width  2
-			  right-margin-width 2
-			  display-line-numbers nil
-			  meow-cursor-type-normal '(bar . 0)
-			  meow-cursor-type-keypad '(bar . 0))
-	      (set-window-buffer (selected-window) (current-buffer)))))
+	        (lambda ()
+	          (with-current-buffer "*scratch*"
+	            (setq-local buffer-read-only   t
+			                    left-margin-width  2
+			                    right-margin-width 2
+			                    display-line-numbers nil
+			                    meow-cursor-type-normal '(bar . 0)
+		                      meow-cursor-type-keypad '(bar . 0))
+	            (set-window-buffer (selected-window) (current-buffer)))))
 
 (provide 'knot-startup-page)
