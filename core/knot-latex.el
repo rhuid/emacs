9f169eb62947c4496e022e63614ac30b81261031
Ronald Huidrom <ronhuidrom@gmail.com>
2025-08-22 21:31:23 +0530

    add abbrevs for latex mode

--- core/knot-latex.el
+++ core/knot-latex.el
@@ -2,40 +2,66 @@
 
 (use-package auctex
   :mode (( "\\.tex\\'" . LaTeX-mode)
-	 ( "\\.cls\\'" . LaTeX-mode)
-	 ( "\\.sty\\'" . LaTeX-mode))
-  ;; :bind
+	       ( "\\.cls\\'" . LaTeX-mode)
+	       ( "\\.sty\\'" . LaTeX-mode))
+  :hook ((post-command . rh/toggle-latex-abbrev))
   :config
   (setq TeX-view-program-selection '((output-pdf "Sioyek")))
   ;; (setq TeX-view-program-list      '(("Sioyek" "sioyek --reuse-instance %o")))
+
+  (defun rh/toggle-latex-abbrev ()
+    "Enable abbrev only outside math mode in LaTeX."
+    (if (and (derived-mode-p 'LaTeX-mode)
+             (texmathp))
+        (abbrev-mode -1)
+      (abbrev-mode 1)))
+
+  ;; Defining abbreviations for LaTeX here
+  (define-abbrev-table 'LaTeX-mode-abbrev-table
+    '(
+      ;; Type x and it becomes $x$ (works for upper-case too)
+      ("p" "$p$" nil 0)
+      ("q" "$q$" nil 0)
+      ("r" "$r$" nil 0)
+      ("s" "$s$" nil 0)
+      ("t" "$t$" nil 0)
+      ("u" "$u$" nil 0)
+      ("v" "$v$" nil 0)
+      ("w" "$w$" nil 0)
+      ("x" "$x$" nil 0)
+      ("y" "$y$" nil 0)
+      ("z" "$z$" nil 0)
+      ))
   )
 
-(use-package auctex-latexmk :after auctex
+(use-package auctex-latexmk
+  :after auctex
   :config
   (auctex-latexmk-setup))
 
 (use-package cdlatex
+  :after auctex
   :hook ((LaTeX-mode . turn-on-cdlatex)
-         (org-mode . turn-on-org-cdlatex))
+         (org-mode   . turn-on-org-cdlatex))
   :config
   (setq cdlatex-env-alist
-	'(("axiom" "\\begin{axiom}\nAUTOLABEL\n?\n\\end{axiom}\n" nil)                 ; AUTOLABEL
-	  ("defintion" "\\begin{definition}\nAUTOLABEL\n?\n\\end{definition}\n" nil)
-	  ("lemma" "\\begin{lemma}\nAUTOLABEL\n?\n\\end{lemma}\n" nil)
-	  ("proposition" "\\begin{proposition}\nAUTOLABEL\n?\n\\end{proposition}\n" nil)
-	  ("theorem" "\\begin{theorem}\nAUTOLABEL\n?\n\\end{theorem}\n" nil)
-	  ("corollary" "\\begin{corollary}\nAUTOLABEL\n?\n\\end{corollary}\n" nil)))
+	      '(("axiom"        "\\begin{axiom}\nAUTOLABEL\n?\n\\end{axiom}\n" nil)                 ; AUTOLABEL
+	        ("defintion"    "\\begin{definition}\nAUTOLABEL\n?\n\\end{definition}\n" nil)
+	        ("lemma"        "\\begin{lemma}\nAUTOLABEL\n?\n\\end{lemma}\n" nil)
+	        ("proposition"  "\\begin{proposition}\nAUTOLABEL\n?\n\\end{proposition}\n" nil)
+	        ("theorem"      "\\begin{theorem}\nAUTOLABEL\n?\n\\end{theorem}\n" nil)
+	        ("corollary"    "\\begin{corollary}\nAUTOLABEL\n?\n\\end{corollary}\n" nil)))
   (setq cdlatex-command-alist
-	'(("ax" "Insert axiom env" "" cdlatex-environment ("axiom") t nil)             ; ax TAB -> axiom env
-	  ("df" "Insert definition env" "" cdlatex-environment ("definition") t nil)   ; df TAB -> definition env
-	  ("lm" "Insert lemma env" "" cdlatex-environment ("lemma") t nil)             ; lm TAB -> lemma env
-	  ("pp" "Insert proposition env" "" cdlatex-environment ("proposition") t nil) ; pp TAB -> propositin env
-	  ("th" "Insert theorem env" "" cdlatex-environment ("theorem") t nil)         ; th TAB -> theorem env
-	  ("cl" "Insert corollary env" "" cdlatex-environment ("corollary") t nil)     ; cl TAB -> corollary env
-	  ("pr" "Insert proof env" "" cdlatex-environment ("proof") t nil))))          ; pr TAB -> proof env
+	      '(("ax"   "Insert axiom env"       "" cdlatex-environment ("axiom") t nil)
+	        ("def"  "Insert definition env"  "" cdlatex-environment ("definition") t nil)
+	        ("lem"  "Insert lemma env"       "" cdlatex-environment ("lemma") t nil)
+	        ("prop" "Insert proposition env" "" cdlatex-environment ("proposition") t nil)
+	        ("th"   "Insert theorem env"     "" cdlatex-environment ("theorem") t nil)
+	        ("cor"  "Insert corollary env"   "" cdlatex-environment ("corollary") t nil)
+	        ("pr"   "Insert proof env"       "" cdlatex-environment ("proof") t nil))))
+
+
 
 (use-package latex-preview-pane)
-
-;;(use-package auto-complete-auctex)
 
 (provide 'knot-latex)
