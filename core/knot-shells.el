2ba2fc9b815237b781799e543178a1022d25debd
Ronald Huidrom <ronhuidrom@gmail.com>
2025-08-31 23:19:00 +0530

    tweaks for eshell

--- core/knot-shells.el
+++ core/knot-shells.el
@@ -2,10 +2,14 @@
 
 ;;;; Eshell
 
-(use-package eshell :ensure nil :demand t
+(use-package eshell
+  :ensure nil
+  :demand t
   :commands eshell
-  :hook ((eshell-first-time-mode . rh/eshell-init)
-	 (eshell-mode . esh-autosuggest-mode))
+  :hook
+  (eshell-first-time-mode . rh/eshell-init)
+  (eshell-mode . esh-autosuggest-mode)
+
   :config
   (defun rh/eshell-toggle ()
     "Toggle the most recent eshell buffer."
@@ -13,36 +17,55 @@
     (let ((eshell-buffer
            (seq-find (lambda (buf)
                        (with-current-buffer buf
-			 (derived-mode-p 'eshell-mode)))
+			                   (derived-mode-p 'eshell-mode)))
                      (buffer-list))))
       (if (eq (current-buffer) eshell-buffer)
           (switch-to-buffer (other-buffer))
-	(if eshell-buffer
+	      (if eshell-buffer
             (switch-to-buffer eshell-buffer)
           (eshell)))))
 
+  ;; Initialization
+
   (defun rh/eshell-init ()
     ;; Set prompt
+
     (setq eshell-prompt-function
-	  (lambda ()
-	    (concat
-	     (propertize (user-login-name) 'face `(:foreground "cyan"))
-	     "@"
-	     (propertize (system-name) 'face `(:foreground "green"))
-	     ":"
-	     (propertize (eshell/pwd) 'face `(:foreground "blue"))
-	     (if (= (user-uid) 0) " # " " $ "))))
+	        (lambda ()
+	          (concat
+	           (propertize (user-login-name) 'face `(:foreground "cyan"))
+	           "@"
+	           (propertize (system-name) 'face `(:foreground "green"))
+	           ":"
+	           (propertize (eshell/pwd) 'face `(:foreground "blue"))
+	           (if (= (user-uid) 0) " # " " $ "))))
+
+    ;; Extra visual touches
     (display-line-numbers-mode -1)
+
+    (with-current-buffer "*eshell*"
+      (setq-local left-margin-width  1
+                  right-margin-width 1)
+      (set-window-buffer (selected-window) (current-buffer)))
     )
+
+  :custom
+  (eshell-history-size 100000)
+  (eshell-prompt-regexp "[$#] ")
   )
 
-(use-package eshell-syntax-highlighting :demand t :after eshell
+(use-package eshell-syntax-highlighting
+  :demand t
+  :after eshell
   :config
   (eshell-syntax-highlighting-global-mode +1))
 
 (use-package esh-autosuggest :demand t :after eshell)
-(use-package eat :after eshell
-  ;; Emulate A Terminal
+
+;; Emulate A Terminal
+
+(use-package eat
+  :after eshell
   :commands (eat eat-eshell-mode)
   :hook (eshell-mode . eat-eshell-mode))
 
