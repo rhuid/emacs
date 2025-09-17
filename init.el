fbac0491c6b40c3b5ce2111b5d40f0105a1d463e
Ronald Huidrom <ronhuidrom@gmail.com>
2025-09-18 04:49:44 +0530

    renaming of knot module

--- init.el
+++ init.el
@@ -78,5 +78,5 @@
 (use-package knot-email             :ensure nil :defer nil)
 (use-package knot-teleport          :ensure nil :defer nil)
 (use-package knot-editor            :ensure nil :defer nil)
-(use-package knot-scratch           :ensure nil
+(use-package knot-special-buffers   :ensure nil
   :commands (rh/toggle-org-scratch rh/toggle-lean-scratch))
