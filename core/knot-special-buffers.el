55e12ba31e8e1628ea9723aaaf05a64b2bedfc73
Ronald Huidrom <ronhuidrom@gmail.com>
2025-09-18 05:00:15 +0530

    add a knot module

---
+++ core/knot-special-buffers.el
@@ -0,0 +1,36 @@
+;;; knot-special-buffers.el --- For special buffers like ibuffer, magit-section, custom scratch buffers, etc. -*- lexical-binding: t; -*-
+
+(defun rh/toggle-scratch-buffer (name mode initial-content)
+  "Toggle a named scratch buffer NAME in MODE with optional INITIAL-CONTENT."
+  (let ((buf (get-buffer name)))
+    (if buf
+        (if (eq (current-buffer) buf)
+            (switch-to-buffer (other-buffer))
+          (switch-to-buffer buf))
+      (let ((new-buf (get-buffer-create name)))
+        (with-current-buffer new-buf
+          (funcall mode)
+          (unless (string= (buffer-string) "") (erase-buffer))
+          (insert initial-content)
+          (goto-char (point-max)))
+        (switch-to-buffer new-buf)))))
+
+(defun rh/toggle-org-scratch ()
+  "Toggle `*org-scratch*` buffer."
+  (interactive)
+  (rh/toggle-scratch-buffer "*org-scratch*" #'org-mode "#+TITLE: Org Scratch\n\n"))
+
+(defun rh/toggle-lean-scratch ()
+  "Toggle `*lean-scratch*` buffer."
+  (interactive)
+  (require 'lean4-mode)
+  (rh/toggle-scratch-buffer "*lean-scratch*" #'lean4-mode "/- Lean Scratch Buffer -/\n\n"))
+
+;;; Optionally redefine default *scratch* as Emacs Lisp
+;; (setq initial-major-mode 'emacs-lisp-mode)
+;; (setq initial-scratch-message ";; Emacs Lisp Scratch Buffer\n\n")
+
+(use-package ibuffer
+  :bind ("C-x C-b" . ibuffer))
+
+(provide 'knot-special-buffers)
