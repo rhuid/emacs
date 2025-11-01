a8f12f45cacfb142328c5a887f169ebbdc0c82db
Ronald Huidrom <ronhuidrom@gmail.com>
2025-11-01 15:42:09 +0530

    add more things

--- custom.el
+++ custom.el
@@ -1,1 +1,14 @@
-(load-library "~/.emacs.d/elpa/puni-20241007.1609/puni.el")
+;; This is my custom file (anyone else can ignore this)
+;; I use this for two purposes
+;; One, to scrap some DIY stuff that are specific to my computer without polluting the rest of my configuration
+;; Two, as a traditional custom file for Emacs to infringe upon (although I almost never use interactive customization)
+
+(load-library "~/.emacs.d/elpa/puni-20241007.1609/puni.el")          ; for some reason, `puni' won't fully load without this
+(load-library "~/.emacs.d/.archives/dictrus.el")                     ; `dictrus' --- https://github.com/rhuid/dictrus
+(bind-key "C-c d d" 'rh/dictrus-lookup)
+
+;; Silently (without output) invoke `nemo' file manager in the current directory.
+(bind-key "C-s-n" (lamb (start-process-shell-command "Nemo" nil "nemo .")))
+
+(use-package achievements :init (achievements-mode))
+(use-package wordel)
