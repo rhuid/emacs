e984ff70a71d53b6b407f22ecab8fdc3ddbe0ca0
Ronald Huidrom <ronhuidrom@gmail.com>
2025-11-01 23:31:26 +0530

    major major cleanup

--- modules/knot-dired.el
+++ modules/knot-dired.el
@@ -1,14 +1,11 @@
-;;; knot-dired.el --- Enchancements of the already great file manager -*- lexical-binding: t; -*-
+;;; knot-dired.el --- Dired is the greatest file manager. Does anybody know that? -*- lexical-binding: t; -*-
 
 (use-package dired
   :ensure nil
   :bind (:map dired-mode-map
-              ("g"    . dired-git-info-mode)
-              ("r"    . dired-up-directory)
-              ("i"    . rh/dired-open-file)
-              ("u"    . dired-unmark)
-              ("U"    . dired-unmark-all-marks)
-              ("<f5>" . revert-buffer-quick))
+              ("b" . dired-up-directory)                  ; `b' for back
+              ("r" . dired-toggle-read-only)              ;  enter `WDired' mode!
+              ("f" . dired-display-file))                 ; `f' for display-`f'ile
   :hook ((dired-mode . rh/dired-setup)
 	       (dired-mode . dired-hide-details-mode)
 	       (dired-mode . dired-click-to-select-mode))
@@ -19,84 +16,13 @@
     (hl-line-mode 1))
   (setq dired-listing-switches "-alh --group-directories-first"
 	      dired-dwim-target t
-	      dired-mouse-drag-files t
-	      dired-recursive-copies 'always))
-
-(use-package all-the-icons-dired
-  :hook (dired-mode . all-the-icons-dired-mode))
-
-(use-package diredfl
-  :hook (dired-mode . diredfl-mode)
-  :config (diredfl-global-mode 1))
-
-(use-package dired-git-info
-  :after dired
-  :bind (:map dired-mode-map (")" . dired-git-info-mode))
-  :config (setq dgi-auto-hide-details-p nil))
-
-(use-package dired-preview :disabled t :after dired
-  :hook (dired-mode . dired-preview-mode)
-  :config
-  (setq dired-preview-delay 0.2
-        dired-preview-max-size 10                   ;; max 10 MB
-	      dired-preview-use-timer t))
-
-(use-package dired-du :disabled t
-  :after dired
-  :config (add-hook 'dired-mode-hook #'dired-du-mode))
-
-(defun open-in-file-manager ()
-  "Open the current directory in the system's GUI file manager."
-  (interactive)
-  (let ((path (shell-quote-argument (expand-file-name default-directory))))
-    (cond ((eq system-type 'windows-nt)
-	         (shell-command (concat "explorer " (replace-regexp-in-string "/" "\\" path t t))))
-	        ((eq system-type 'darwin)
-	         (shell-command (concat "open " path)))
-	        ((eq system-type 'gnu/linux)
-	         (shell-command (concat "xdg-open " path))))))
-
-(defun rh/dired-open-file ()
-  "Smart open for files in Dired:
-- Directories: open in Dired.
-- Known external files (e.g., media, PDF): open externally.
-- Audio files: play with EMMS.
-- All others: open in Emacs side window."
-  (interactive)
-  (let ((file (dired-get-file-for-visit)))
-    (cond
-     ;; Directories: open in same buffer
-     ((file-directory-p file)
-      (dired-find-alternate-file))
-
-     ;; Audio files: play with EMMS
-     ((string-match-p
-       (rx (or ".mp3" ".flac" ".wav" ".m4a" ".ogg" ".opus") eos)
-       file)
-      (emms-play-file file))
-
-     ;; CBZ: open with yacreader
-     ((string-match-p
-       (rx (or ".cbz") eos)
-       file)
-      (start-process "com.yacreader.YACReader" nil "com.yacreader.YACReader" file))
-
-     ;; External files: open with system default
-     ((string-match-p
-       (rx (or ".mp4" ".mkv" ".avi"
-               ".jpg" ".jpeg" ".png" ".gif" ".svg"
-               ".pdf" ".epub" ".cbr") eos)
-       file)
-      (cond ((eq system-type 'windows-nt)
-             (shell-command (concat "start \"\"" (shell-quote-argument file))))
-            ((eq system-type 'darwin)
-             (shell-command (concat "open " (shell-quote-argument file))))
-            ((eq system-type 'gnu/linux)
-             (shell-command (concat "xdg-open " (shell-quote-argument file))))))
-
-     ;; Everything else: open in Emacs side window
-     (t
-      ;; (display-buffer (find-file-noselect file))
-      (dired-display-file)))))
+	      dired-mouse-drag-files t                                         ; drag and drop files with mouse
+	      dired-recursive-copies 'always)
+  (use-package diredfl :init (diredfl-global-mode))                      ; make dired look better
+  (use-package all-the-icons-dired                                       ; icons for dired buffer
+    :hook (dired-mode . all-the-icons-dired-mode))
+  (use-package dired-git-info                                            ; show git info in dired
+    :bind (:map dired-mode-map (")" . dired-git-info-mode))
+    :config (setq dgi-auto-hide-details-p nil)))
 
 (provide 'knot-dired)
