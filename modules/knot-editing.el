c231ef0da2f19a32b1592ed7d3072e60d874ab71
Ronald Huidrom <ronhuidrom@gmail.com>
2025-11-04 17:10:53 +0530

    add rh/join-line

--- modules/knot-editing.el
+++ modules/knot-editing.el
@@ -1,19 +1,13 @@
 ;;; knot-editing.el --- Some commands for more efficient editing -*- lexical-binding: t; -*-
 
-(defun rh/open-line-below ()
-  "Create a new line below and move the cursor there."
-  (interactive)
-  (move-end-of-line 1)
-  (newline t t))
+(defun rh/join-line (&optional ARG)
+  "Join the current line to the following ARG lines and fix up whitespace at join. ARG defaults to 1."
+  (interactive "p")
+  (save-excursion
+    (dotimes (i ARG)
+      (join-line -1))))
 
-(defun rh/open-line-above ()
-  "Create a new line above and move the cursor there."
-  (interactive)
-  (mwim-beginning-of-code-or-line)
-  (open-line 1))
-
-(bind-key "C-<return>" 'rh/open-line-below)
-(bind-key "C-S-<return>" 'rh/open-line-above)
+(bind-key "C-j" 'rh/join-line)
 
 (defun rh/chop-off-buffer (&optional ARG)
   "Kill the rest of the buffer after point (or before point with prefix ARG)."
@@ -42,5 +36,20 @@
     (kill-ring-save (region-beginning) (region-end))))
 
 (bind-key "C-@" 'rh/copy-sentence)
+
+(defun rh/open-line-below ()
+  "Create a new line below and move the cursor there."
+  (interactive)
+  (move-end-of-line 1)
+  (newline t t))
+
+(defun rh/open-line-above ()
+  "Create a new line above and move the cursor there."
+  (interactive)
+  (mwim-beginning-of-code-or-line)
+  (open-line 1))
+
+(bind-key "C-<return>" 'rh/open-line-below)
+(bind-key "C-S-<return>" 'rh/open-line-above)
 
 (provide 'knot-editing)
