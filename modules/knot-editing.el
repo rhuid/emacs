bee45f77e7372e062cef1e018c48ffe13e3698ea
Ronald Huidrom <ronhuidrom@gmail.com>
2025-11-04 18:22:47 +0530

    improve several commands

--- modules/knot-editing.el
+++ modules/knot-editing.el
@@ -10,18 +10,18 @@
 (bind-key "C-j" 'rh/join-line)
 
 (defun rh/chop-off-buffer (&optional ARG)
-  "Kill the rest of the buffer after point (or before point with prefix ARG)."
-  (interactive "P")
-  (if ARG
-      (kill-region (point-min) (point))
-    (kill-region (point) (point-max))))
-
-(defun rh/backward-chop-off-buffer (&optional ARG)
-  "Kill the rest of the buffer before point (or after point with prefix ARG)."
+  "Kill the rest of the buffer after point. With ARG, it deletes instead (does not save to the kill-ring)."
   (interactive "P")
   (if ARG
       (kill-region (point) (point-max))
-    (kill-region (point-min) (point))))
+    (delete-region (point) (point-max))))
+
+(defun rh/backward-chop-off-buffer (&optional ARG)
+  "Kill the rest of the buffer before point. With ARG, it deletes instead (does not save to the kill-ring)."
+  (interactive "P")
+  (if ARG
+      (kill-region (point-min) (point))
+    (delete-region (point-min) (point))))
 
 (bind-key "C-M-S-k" 'rh/chop-off-buffer)
 (bind-key "C-M-S-h" 'rh/backward-chop-off-buffer)
@@ -37,17 +37,17 @@
 
 (bind-key "C-@" 'rh/copy-sentence)
 
-(defun rh/open-line-below ()
-  "Create a new line below and move the cursor there."
-  (interactive)
+(defun rh/open-line-below (&optional ARG)
+  "Create a new line below and move the cursor there. With ARG, perform this action that many times."
+  (interactive "p")
   (move-end-of-line 1)
-  (newline t t))
+  (newline-and-indent ARG))
 
-(defun rh/open-line-above ()
-  "Create a new line above and move the cursor there."
-  (interactive)
+(defun rh/open-line-above (&optional ARG)
+  "Create a new line above and move the cursor there. With ARG, perform this action that many times."
+  (interactive "p")
   (mwim-beginning-of-code-or-line)
-  (open-line 1))
+  (open-line ARG))
 
 (bind-key "C-<return>" 'rh/open-line-below)
 (bind-key "C-S-<return>" 'rh/open-line-above)
