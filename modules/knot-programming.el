f22c26536669626a2612a3a758cbbc3ece50cb4c
Ronald Huidrom <ronhuidrom@gmail.com>
2025-11-19 09:50:26 +0530

    trivial update

--- modules/knot-programming.el
+++ modules/knot-programming.el
@@ -70,7 +70,6 @@
 (use-package nix-mode)
 (use-package rust-mode)
 (use-package systemd)
-(use-package ucl-mode :ensure nil)
 (use-package csv-mode :hook (csv-mode . csv-align-mode))
 
 (provide 'knot-programming)
