2ca3cd26fa7236f5c7844ecf96e22e50d0702a8c
Ronald Huidrom <ronhuidrom@gmail.com>
2025-11-20 02:15:48 +0530

    * README.org: More org babel for literate config

--- early-init.el
+++ early-init.el
@@ -1,32 +1,32 @@
 ;;; early-init.el --- Load this before init.el -*- lexical-binding: t; -*-
 
-;; Defer garbage collection to speed up startup.
+;; Defer garbage collection
 (setq gc-cons-threshold most-positive-fixnum
       gc-cons-percentage 0.6)
 
-;; Set font and start frame in full screen.
 (setq default-frame-alist '((font . "Iosevka Term-15") (fullscreen . maximized)))
 
-;; Disable UI elements.
+;; Disable UI elements
 (menu-bar-mode   -1)
 (tool-bar-mode   -1)
 (scroll-bar-mode -1)
 (tooltip-mode    -1)
 (set-fringe-mode -1)
 
+;; Inhibit startup annoyances
 (setq
- inhibit-startup-screen  t                              ; inhibit the default `Welcome to GNU Emacs' buffer
- inhibit-default-init    t                              ; don't load `default.el' from the Emacs installation directory
+ inhibit-startup-screen  t                  ; inhibit the default `Welcome to GNU Emacs' buffer
+ inhibit-default-init    t                  ; don't load `default.el' from the Emacs installation directory
  inhibit-startup-message t
- inhibit-x-resources     t                              ; inhibit loading X resources (faster on X11)
+ inhibit-x-resources     t                  ; inhibit loading X resources (faster on X11)
  inhibit-compacting-font-caches t
  inhibit-startup-echo-area-message t
- frame-inhibit-implied-resize t                         ; don't automatically resize frame when changing font or UI elements
- server-client-instructions nil                         ; make client sessions cleaner
- emacs-start-time (current-time)                        ; record the time Emacs started, for profiling startup time later
- warning-minimum-level :error)                          ; don't pop up a warning buffer unless it's an error
+ frame-inhibit-implied-resize t             ; don't automatically resize frame when changing font or UI elements
+ server-client-instructions nil             ; make client sessions cleaner
+ emacs-start-time (current-time)            ; record the time Emacs started, for profiling startup time later
+ warning-minimum-level :error)              ; don't pop up a warning buffer unless it's an error
 
-(fset 'display-startup-echo-area-message #'ignore)      ; suppress the minibuffer message shown at startup
+(fset 'display-startup-echo-area-message #'ignore)    ; suppress the minibuffer message shown at startup
 
 ;; Native compilation tweaks
 (setq package-native-compile t
